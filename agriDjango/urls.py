86d1401eadad3f1eec8c105cd299ff8e135acd0a
MohamedMokhtarHadded <mohamedmokhtar.hadded@esprit.tn>
2024-10-29 17:11:45 +0100

    ai

--- agriDjango/urls.py
+++ agriDjango/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from django.urls import path
 from climate import views, ai
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('template/tables', views.template_tables, name='home'),
 
+    #mokhtar api
+    path('api/', include('predictions.urls')),
+
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
 
 
     # path('weather/', views.fetch_weather_data, name='weather'),
@@ -18,5 +22,5 @@ urlpatterns = [
     path('fields/create/', views.create_field, name='create_field'),
     path('fields/', views.field_list, name='field_list'),
     path('fields/<int:field_id>/delete/', views.field_delete, name='field_delete'),
-    path('fields/<int:field_id>/update/', views.field_update, name='field_update'),  # Update URL
+    path('fields/<int:field_id>/update/', views.field_update, name='field_update'), 
 ]
